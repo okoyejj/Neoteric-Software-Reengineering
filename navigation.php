b3e04647232a46f2d4455207ab950af2bf315f7d
okoyejj <okoyejj@hotmail.com>
2017-03-14 21:38:25 +0000

    Dropdown working
    Overflow:hidden was the culprit as the Nav was fixed.

--- navigation.php
+++ navigation.php
@@ -1,11 +1,13 @@
 <nav>
 	<ul>
 	<li><a class="active" href="#home">Home</a></li>
-	<li class="tech"><a href="#tutorials">Technologies</a>
+	<li class="dropdown"><a class="btn" href="#tutorials">Technologies</a>
 		<div class="navi">
-			<a href="#">Alpha</a>
-			<a href="#">Beta</a>
-			<a href="#">Gamma</a>
+			<a href="#">JavaScript and Derivatives</a>
+			<a href="#">PHP and CakePHP</a>
+			<a href="#">Oracle and SQL</a>
+			<a href="#">BootStrap</a>
+			<a href="#">HTML5 and CSS3</a>
 		</div>
 	</li>
 	<li><a href="#contact">Contact</a></li>
