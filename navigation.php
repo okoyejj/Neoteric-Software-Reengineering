23944e48540ceb11f727a52d8de0b8a746d9844b
okoyejj <okoyejj@hotmail.com>
2017-03-14 20:12:32 +0000

    Navigation drop down test 1
    Testing the navigation bar to make it work

--- navigation.php
+++ navigation.php
@@ -1,7 +1,13 @@
 <nav>
 	<ul>
 	<li><a class="active" href="#home">Home</a></li>
-	<li><a href="#tutorials">Technologies</a></li>
+	<li class="tech"><a href="#tutorials">Technologies</a>
+		<div class="navi">
+			<a href="#">Alpha</a>
+			<a href="#">Beta</a>
+			<a href="#">Gamma</a>
+		</div>
+	</li>
 	<li><a href="#contact">Contact</a></li>
 	<li><a href="#gallery">Gallery</a></li>
 	<li><a href="#about">About</a></li>
